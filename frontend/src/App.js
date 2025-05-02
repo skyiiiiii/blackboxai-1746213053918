2475d4501469e82f0fa14a9bfdb8984271c6b8ad
Yash Raj <yashchoudhary10a@gmail.com>
2025-05-03 00:41:20 +0530

    Add frontend/src/App.js 1746213079756

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+import './index.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
